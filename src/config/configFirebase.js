217ab30626ba148f92be75f9fb729f4f7a0fdbf4
Yaroslav Melmychenko <yaroslav.melnychenko@chisw.com>
2019-03-21 14:24:48 +0200

    set up firebase, dotenv

---
+++ src/config/configFirebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+var config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "planning-app-cecff.firebaseapp.com",
+  databaseURL: "https://planning-app-cecff.firebaseio.com",
+  projectId: "planning-app-cecff",
+  storageBucket: "planning-app-cecff.appspot.com",
+  messagingSenderId: "714438655888"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({timestampsInSnapshots: true});
+
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import thunk from 'redux-thunk';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
+require('dotenv').config()
+
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
